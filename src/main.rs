282ee3737bb93d781d773e9de6c8956cf67fe552
Marcus Rådell <marcus@radell.net>
2024-02-12 19:34:47 +0100

    /status should not return any content

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/status", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/status", get(|| async {}));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
