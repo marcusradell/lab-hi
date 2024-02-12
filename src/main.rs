f9c672838520268594aca4762c5064ae406a07ed
Marcus Rådell <marcus@radell.net>
2024-02-12 19:42:17 +0100

    api/greeter/hi returns Hi!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/status", get(|| async {}));
+    let app = Router::new()
+        .route("/status", get(|| async {}))
+        .route("/api/greeter/hi", get(|| async { return "Hi!" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
