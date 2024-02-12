ab7b26c1b94861f154b544ae1104aa93d29e8de5
Marcus Rådell <marcus@radell.net>
2024-02-12 19:35:32 +0100

    remove code comments

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
     let app = Router::new().route("/status", get(|| async {}));
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
